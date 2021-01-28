e798f1e1b7a1309c04f1c543161d274ae5b1b521
Ben Mason <locutus@the-collective.net>
2021-01-28 15:01:16 -0500

    Added Ability to use a custom char table

--- b64.py
+++ b64.py
@@ -1,4 +1,4 @@
-__author__ = ('Leonardo F Oliveira')
+__author__ = ('Ben Mason')
 
 __all__ = ['encode', 'decode']
 
@@ -11,8 +11,14 @@ class b64:
     def __str__(self):
         return 'Base64 Encoder / Decoder'
 
-    def encode(self, text):
+    def encode(self, text, alttable=''):
         bins = str()
+
+        if alttable != '':
+            char_table = alttable
+        else:
+            char_table = self.table
+
         for c in text:
             bins += '{:0>8}'.format(str(bin(ord(c)))[2:])
         while len(bins) % 3:
@@ -28,16 +34,23 @@ class b64:
             if b == '000000':
                 base64 += '='
             else:
-                base64 += self.table[int(b, 2)]
+                base64 += char_table[int(b, 2)]
         return base64
 
-    def decode(self, text):
+    def decode(self, text, alttable=''):
         bins = str()
+
+        if alttable != '':
+            char_table = alttable
+        else:
+            char_table = self.table
+
+
         for c in text:
             if c == '=':
                 bins += '000000'
             else:
-                bins += '{:0>6}'.format(str(bin(self.table.index(c)))[2:])
+                bins += '{:0>6}'.format(str(bin(char_table.index(c)))[2:])
         for i in range(8, len(bins) + int(len(bins) / 8), 9):
             bins = bins[:i] + ' ' + bins[i:]
         bins = bins.split(' ')
